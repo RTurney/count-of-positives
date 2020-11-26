71c6ed8886c124d773b967289d1bdcc1c618b3fa
Richard Turney <r.turney93@gmail.com>
2020-11-26 21:03:09 +0000

    Removed tests from code

--- count-of-positives.rb
+++ count-of-positives.rb
@@ -5,9 +5,3 @@ def count_positives_sum_negatives(lst = nil)
   lst.each { |x| x > 0 ? (count += 1) : (sum += x)}
   return [count, sum]
 end
-
-
-
-#tests
-puts count_positives_sum_negatives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15])
-puts count_positives_sum_negatives()
