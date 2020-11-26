bdcd8b6f48d093dae7443633b65f902a64b5efd0
Richard Turney <r.turney93@gmail.com>
2020-11-26 21:20:53 +0000

    Final submission

--- count-of-positives.rb
+++ count-of-positives.rb
@@ -1,5 +1,5 @@
-def count_positives_sum_negatives(lst = nil)
-  return [] if lst.nil?
+def count_positives_sum_negatives(lst)
+  return [] if lst.nil? || lst.length == 0
   count = 0
   sum = 0
   lst.each { |x| x > 0 ? (count += 1) : (sum += x)}
